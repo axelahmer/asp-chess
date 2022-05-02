5aec67b6c15554f0de670cefc823400b53c99cc6
Kee Chew Huang(S3808292) <s3808292@student.rmit.edu.au>
2022-05-02 13:52:03 +1000

    update python script to print all the solutions

--- printChessBoard.py
+++ printChessBoard.py
@@ -1,3 +1,5 @@
+import os
+
 def read_prolog(arr, dimension):
     board = [[" "] * dimension for _ in range(8)]
     for chess in arr:
@@ -24,11 +26,33 @@ def print_board(board, dimension):
             print(board[dimension-1-i][j], end=" |")
     print(linebreak)
 
+
+def run_clingo(n):
+    os.system("clingo chess.lp --const n={} 0 > output.txt".format(n)) 
+
+def read_output_and_print(n):
+    with open("output.txt", "r") as reader:
+        results = reader.readlines()
+        ans_count = 0
+        for i in range(len(results)):
+            if results[i].startswith("Answer"):
+                ans_count += 1
+                i += 1
+                arr = results[i].split()
+                board = read_prolog(arr, n)
+                print("\nAnswer {}".format(ans_count) )
+                print_board(board, n)
+
+
 if __name__ == "__main__":
-    val = ""
-    while val != "q":
-        val = input("Enter the ASP result or q to quit\n")
-        dimension = int(input("Enter dimension of the chessboard\n"))
-        arr = val.split()
-        board = read_prolog(arr, dimension)
-        print_board(board, dimension)
+    # val = ""
+    # while val != "q":
+    #     val = input("Enter the ASP result or q to quit\n")
+    #     dimension = int(input("Enter dimension of the chessboard\n"))
+    #     arr = val.split()
+    #     board = read_prolog(arr, dimension)
+    #     print_board(board, dimension)
+
+    n = int(input("Enter board dimension \n"))
+    run_clingo(n)
+    read_output_and_print(n)
